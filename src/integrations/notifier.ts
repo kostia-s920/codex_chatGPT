ce3a113889efaf0e38e2d494777a28af4cb0ec1b
kostia-s920 <kostia.s920@gmail.com>
2026-05-28 13:24:28 +0300

    Delete src directory

--- src/integrations/notifier.ts
+++
@@ -1,9 +0,0 @@
-export interface Notifier {
-  send(message: string): Promise<void>;
-}
-
-export class NoopSlackNotifier implements Notifier {
-  async send(_message: string): Promise<void> {
-    // Reserved extension point for future Slack support.
-  }
-}
